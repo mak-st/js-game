33b028e7e8d18bd60a9a885de567b180240a14a5
mak-st <vincentmst24@gmail.com>
2019-06-12 23:34:17 +0800

    Update 23:34
    Update game

--- cat/cat.js
+++ cat/cat.js
@@ -1,8 +1,16 @@
 const cat = document.getElementById('cat');
 const c = cat.getContext('2d');
 
+const cat1 = new Image();
+cat1.src = 'img/cat1.png';
+const cat2 = new Image();
+cat2.src = 'img/cat2.png';
+
+
 const cw = cat.width;
 const ch = cat.height;
+
+var started = false;
 
 class Rect
 {
@@ -32,27 +40,52 @@ class Rect
 
 }
 
-function drawButton() {
-	var button = new Rect;
-	button.x = cw/2;
-	button.y = ch*3/4;
-	button.w = 200;
-	button.h = 100;
-	drawRect('white', button.left, button.top, button.w, button.h);
+var startBtn = new Rect(cw/2, ch*3/4, 200, 100);
+
+function drawButton(button) {
+	drawRect('black', button.left, button.top, button.w, button.h);
+	c.fillStyle = 'white';
+	c.font = '30px Arial';
+	c.textAlign = 'center';
+	c.textBaseline = 'middle';
+	c.fillText('Start Game', button.x, button.y);
 }
 
 function drawBackground() {
-	drawRect('black', 0, 0, cw, ch);
+	drawRect('white', 0, 0, cw, ch);
 }
 
 function draw() {
 	drawBackground();
-	drawButton();	
+	if (started == false) {
+		drawButton(startBtn);
+	} else {
+		game();
+	}
+		
+}
+var i = 0;
+function catTalk() {
+	i++;
+	if (i%2 == 1) {
+		c.drawImage(cat1, cw/2 - 80, ch/3);
+	} else {
+		c.drawImage(cat2, cw/2 - 80, ch/3);
+	}
 }
 
 function drawRect(color, x, y, w, h) {
 	c.fillStyle = color;
 	c.fillRect(x,y,w,h);
+}
+
+function game() {
+	c.fillStyle = 'black';
+	c.font = '30px';
+	c.textAlign = 'center';
+	c.textBaseline = 'middle';
+	c.fillText('Cat: Meow!', cw / 2, ch *2 / 3);
+	setInterval(catTalk(), 100);
 }
 
 function startPage() {
@@ -65,3 +98,12 @@ function update() {
 }
 
 update();
+
+cat.addEventListener('click', event => {
+	if (event.offsetX >= startBtn.left &&
+		event.offsetX <= startBtn.right &&
+		event.offsetY >= startBtn.top &&
+		event.offsetY <= startBtn.bottom) {
+		started = true;
+	}
+});
